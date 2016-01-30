b437ae67aafc8d3fa1b448172d91a1aa85e223fc
zekelong <zeke_long@hotmail.com>
2016-01-29 18:38:58 -0800

    Create Divisions.cs

---
+++ Models/Divisions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using TARS.Helpers;
+using TARS.Models;
+
+//Necessary for DB calls.
+using System.Data.Entity;
+
+namespace TARS.Models
+{
+    public class Divisions
+    {
+        public int ID { get; set; } //DB iterator
+        public string divName { get; set; }     //Division Name
+    }
+
+    public class DivisionsDBContext : DbContext
+    {
+        public DbSet<Divisions> DivisionsList { get; set; }
+        protected HistoryDBContext HistDB = new HistoryDBContext();
+
+        public int SaveChanges()
+        {
+            //call our code here
+            var hist = new History();
+            ChangeTracker.DetectChanges();
+            var holder = ChangeTracker.Entries<Divisions>();
+            foreach (System.Data.Entity.Infrastructure.DbEntityEntry<Divisions> entry in holder)
+            {
+                switch (entry.State)
+                {
+                    case System.Data.EntityState.Added:
+                        hist.type = "added";
+                        break;
+                    case System.Data.EntityState.Deleted:
+                        hist.type = "deleted";
+                        break;
+                    case System.Data.EntityState.Modified:
+                        hist.type = "modified";
+                        break;
+                }
+                hist.dbtable = "Divisions";
+                hist.change = "Division: " + entry.Property(u => u.divName).CurrentValue;
+            }
+
+            hist.username = System.Web.HttpContext.Current.User.Identity.Name;
+            hist.timestamp = System.DateTime.Now;
+            HistDB.HistoryList.Add(hist);
+            HistDB.SaveChanges();
+
+            return base.SaveChanges();
+        }
+    }
+}
